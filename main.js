3e6fdb09387c1c6cc8d1d33e2728c0d839125a67
Astuti08 <astutisolihatunnisa1502@gmail.com>
2023-10-12 20:40:52 +0700

    add main.js

---
+++ main.js
@@ -0,0 +1,49 @@
+//console.log("Hello World")
+//alert  ("Ada Notifikasi")
+//prompt ("Pinjam Seratus")
+
+//var Promnet="Coding is easy"
+//console.log(Promnet)
+//var Promnet="Coding is easy"
+//console.log(Promnet)
+//var Promnet="nothing is possible"
+//console.log (Promnet)
+
+//let Promnet="Coding is fun"
+//console.log(Promnet)
+//let Promnet="Coding not fun"
+//console.log(Promnet)
+
+//const Promnet="Coding is my life"
+//console.log(Promnet)
+//const Promnet="Coding is not my life"
+//console.log(Promnet)
+
+//let totalPoin=prompt("Masukkan poin anda")
+    //if(totalPoin>=100){
+        //document.write("congratulation")
+    //}else{
+        //document.write("Keep Triying")
+    //}
+
+//let x=6;
+//let y=3;
+
+//console.log(x<10&&y>1)
+//console.log(x<10&&y<1)
+//console.log(x==5||y==5)
+//console.log(x==6||y==5)
+//!(x==y)
+
+// document.write(x<10&&y>1)
+// document.write("<br>");
+// document.write(x<10&&y<1)
+
+//let p=document.querySelector("p")
+//let button=document.querySelector("button")
+//let input=document.querySelector("input")
+//button.addEventListener('click' function())
+//let isi=input.value
+//console.log(isi)
+//p.inert-HTML == isi
+
